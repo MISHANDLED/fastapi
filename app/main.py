615ef9df3082b568b2d2e0e57ba0e54576490c38
mishandled <devanshmohata@outlook.com>
2021-11-18 13:06:29 +0530

    Included CORS and Requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routes_to import users
@@ -10,6 +12,15 @@ from .routes_to import votes
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(posts.router)
 app.include_router(users.router)
